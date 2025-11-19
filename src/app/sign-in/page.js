2d05427d8287201c1858bfbec6380daec3606c72
Fabian Ouwehand <woshifabian@s-MacBook-Pro.local>
2025-11-19 12:26:21 +0800

    Add redirect for Clerk

--- src/app/sign-in/page.js
+++ src/app/sign-in/page.js
@@ -2,8 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
-      <SignIn />
+    <div className="flex justify-center items-center min-h-screen p-4">
+      <SignIn redirectUrl="/dashboard" />
     </div>
   );
 }
--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -2,8 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
-      <SignUp />
+    <div className="flex justify-center items-center min-h-screen p-4">
+      <SignUp redirectUrl="/dashboard" />
     </div>
   );
 }
